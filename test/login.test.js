9d98c1342c2b8db354367e7efc6a4a426a149f27
Pedro Senna <pedro.senna@onbusdigital.com.br>
2025-07-06 19:41:47 -0300

    iniciando o projeto

---
+++ test/login.test.js
@@ -0,0 +1,27 @@
+const request = require('supertest')
+const {expect} = require('chai')
+
+describe('login',()=>{
+    describe('POST', () => {
+        it('deve retornar 200 com um token em string quando usar credenciais validas', async () =>{
+            const response = await request('http://localhost:3000')
+                .post('/login')
+                .set('content-type','application/json')
+                .send({
+                        "username": "julio.lima",
+                        "senha": "123456"
+                    })
+            console.log(response.body)
+            expect(response.status).to.eq(200)
+            expect(response.body.token).to.be.a('string')
+
+
+        })
+
+
+
+    })
+
+
+
+})
